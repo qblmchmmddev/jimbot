a608f0b26269e840a019d9f3d5dfeade18f12046
Mochammad Iqbal <mochammadiqbal@Mochammads-iMac.local>
2022-04-25 04:44:28 +0700

    fix dma, improve ppu irq

--- jimbot-web-wasm/jimbot_web.js
+++ jimbot-web-wasm/jimbot_web.js
@@ -482,7 +482,7 @@ async function init(input) {
     imports.wbg.__wbindgen_throw = function(arg0, arg1) {
         throw new Error(getStringFromWasm0(arg0, arg1));
     };
-    imports.wbg.__wbindgen_closure_wrapper45 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper47 = function(arg0, arg1, arg2) {
         const ret = makeMutClosure(arg0, arg1, 7, __wbg_adapter_20);
         return addHeapObject(ret);
     };
